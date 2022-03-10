be006980e5af2baca454146f1c629ffa5c6b7395
Umutcan Yavuz <36381833+addada123@users.noreply.github.com>
2022-03-10 21:29:03 +0300

    Update randomNumber10.m

--- randomNumber10.m
+++ randomNumber10.m
@@ -7,15 +7,15 @@ test_number = 100000000;
 total_times = 0;
 
 while test < test_number
-    random_number = randi(85);
-    random_number_2 = randi(85);
+    random_number = randperm(85, 1);
+    random_number_2 = randperm(85, 1);
     if random_number == random_number_2
         total_times = total_times + 1;
     end
     test = test + 1;
 end
 
-theorical_probability = (1/85) * (1/85);
+theorical_probability = (1/85) * (1/84);
 practical_probability = total_times / test_number;
 percent_error = abs(theorical_probability - practical_probability) / (abs(theorical_probability) * 100);
 
